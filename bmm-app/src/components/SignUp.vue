34f381f6e465dbad8ca10e401d6ac2a7fdac30e5
Gyasi11 <glcalhoun1123@yahoo.com>
2021-09-19 17:07:54 -0400

    Getting started

---
+++ bmm-app/src/components/SignUp.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="main">
+    <h1>Welcome to Signup Page </h1>
+    <input v-model="username">
+    <p> {{ username  }}</p>
+    <Workout :msg="username" :workouts="workouts" />
+    <button v-on:click="getUsername"> Submit </button>
+  </div>
+</template>
+
+<script>
+ import Workout from './Workout.vue'
+export default {
+  name: 'SignUp',
+ data: function() {
+     return {
+         username: 'londo farmer',
+         workouts: [
+           1,2
+         ]
+     }
+ },
+ components: {
+     Workout,
+  },
+ methods: {
+    getUsername: function() {
+        console.log(this.username)
+        if(this.username !== 'Londo') {
+            this.username = "invalid"
+        }
+
+
+    }
+ },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ bmm-app/src/components/Workout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h4 id="h4-style">Username: {{ msg }}</h4>
+    <ul>
+    <li> {{workouts}} </li>
+    </ul>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'Workout',
+  props: {
+    msg: String,
+    workouts: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#h4-style {
+  font-size: 40px
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
